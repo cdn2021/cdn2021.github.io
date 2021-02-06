1cd742277fe6d0a28b681e8eb9a0b45b44e9efbc
cdn2021 <78522762+cdn2021@users.noreply.github.com>
2021-02-06 17:05:02 +0800

    Update check.js

--- GE/js/pre0.01/check.js
+++ GE/js/pre0.01/check.js
@@ -19,5 +19,15 @@
 		location.href = "https://cdn2021.github.io/GE/support";
 	}
 
-	
+
+	console.log("%c\n       ", "font-size:41px;background:url('https://pig-cmd.github.io/eso1242a/eso1242a-screen.jpg') no-repeat -135px -1px");
+	console.log("%c\n       ", "font-size:41px;background:url('https://cdn2021.github.io/GE/img/earth.bmp') no-repeat -135px -1px");
+	console.log("%cGalactic Empire","font-size:3em;background:black;color:white");
+	console.log("Do you want to work with us to develop web 3D games?");
+	console.log("Do you want to enhance your programming skills?");
+	console.log("Do you want to learn something new?");
+	console.log("Do you want to add a rich ink color to your resume?");
+	console.log("Join us and develop together!");
+	console.log("Air Studios 2021 Recruitment Entry:%sAir Studios 2021 Recruitment Entrance: fat-pig-2020@outlook.com","font-weight:bold;color:red");
+	console.log("Please declare in the message that you are from %sthe web console","font-weight:bold;color:red");
 }
