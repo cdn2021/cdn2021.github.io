19fdfaa8d093a2f361ec3fd72206e8ca3e92e853
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-02-19 10:53:23 +0800

    Update user.js

--- GE/js/standard1.0/user.js
+++ GE/js/standard1.0/user.js
@@ -38,7 +38,7 @@ function reg()
   {
     alert("The user name should not contain a password, or the user name should be part of the password");
     return;
-  }
+  }/*
   var arr = [123,456,789,"abc","qwerty","ABC","QWERTY",111,222,333,444,555,666,777,888,999,987,654,321,520,1314,"password","Password","apple","Apple","PASSWORD","APPLE","admin","Admin","ADMIN",314,15926,"qazwsx","QAZWSX","meiyoumima","Hello World","iloveyou","Iloveyou","ILOVEYOU","def","ghi","xyz","cn","sh","aaa","bbb","ccc","ddd","zzz","xxx","yyy","a1b2c3","A1B2C3","adobe","microsoft","ADOBE","MICROSOFT","letmein","LETMEIN","photoshop","PHOTOSHOP","dog","cat","DOG","CAT","MONKEY","monkey","shadow","SHADOW","sun","SUN","princess","PRINCESS","azerty","AZERTY",000,"trust","TRUST","NO","no","YES","yes","hello","Hello","HELLO",2019,2020,2021,2000,1999,1920,1980,147258369,"a1b2","c3d4","e5f6","t5r4","e3w2","asdfgh","zxcvbn","mnbvcx","lkjhgf","poiuyt","POIUYT","LKJHGF","MNBVCX","ZXCVBN","qq","wechat","ali","tencent","bilibili","youku","netease","baidu","google","youtube","facebook","...","mmm","java","JAVA","python","PYTHON","JS","js","SB","sb","3Q","3q","thank","THANK","YOU","you","250","Script","script","SCRIPT","AJAX","ajax","xml","XML","XHR","xhr","response","Response","RESPONSE","request","REQUEST","go","GO","Go","earth","Earth","EARTH","Star","star","STAR","US","us","CP","Cp","cp","uk","UK","Uk","CN","cn","Cn","PRC","prc","Prc","window","WINDOW","INC","inc","mac","MAC","IOS","ios","Bing","bing","BING","Office","OFFICE","office"]
   console.log(arr);
   var i = 0;
@@ -50,7 +50,7 @@ function reg()
         return;
     }
     i++;
-  }
+  }*/
   let userinfo = {};
   userinfo.username = username;
   userinfo.password = password;
