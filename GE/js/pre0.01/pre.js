c4026109ad739db196030f9aa9f8f80449913890
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-02-19 10:47:53 +0800

    Update pre.js

--- GE/js/pre0.01/pre.js
+++ GE/js/pre0.01/pre.js
@@ -6,14 +6,25 @@ if (localStorage.getItem("key") == null)
 //Set the hot key.
 {
 	let xhr = new XMLHttpRequest();
-	xhr.open("GET","https://cdn2021.github.io/GE/json/star-position.txt?from=GE&version=pre0.1&get=star-position&time=" + new Date().getTime() + "&id=" + Math.random(),true);
+	xhr.open("GET","https://cdn2021.github.io/GE/json/star-position.txt?from=GE&version=pre0.01&get=star-position&time=" + new Date().getTime() + "&id=" + Math.random(),true);
 	xhr.send();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.status == 200)
 			sessionStorage.setItem("star-position",xhr.responseText);
-	}
+	};
 	//Get the position of the stars and save the data.
 }
 if (localStorage.getItem("health") == null)
 	localStorage.setItem("health",1920);
+if (localStorage.getItem("world-block") == null)
+{
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET","https://cdn2021.github.io/GE/json/world-block.txt?from=GE&version=pre0.01&get=world-block&time=" + new Date().getTime() + "&id=" + Math.random(),true);
+	xhr.send();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200)
+			localStorage.setItem("world-block",xhr.responseText);
+	//Get the block of the world.
+	};
+}
 	
